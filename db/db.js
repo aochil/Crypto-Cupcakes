72a447dc7c50e6f6240ff4804b7d07da345314a8
aochil <azizbek.ochilov2@gmail.com>
2022-09-16 13:04:12 -0400

    done

--- db/db.js
+++ db/db.js
@@ -1,11 +1,10 @@
 const path = require('path');
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: path.join(__dirname, 'db.sqlite'),
-    logging: false
-});
+const sequelize = new Sequelize('crypto_cupcakes', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql',
+  });
 
 module.exports = {
     sequelize,
